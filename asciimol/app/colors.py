fdf231a209d53845bc98eba93ed588b18c0ca787
Dominik Behrens <63843135+dewberryants@users.noreply.github.com>
2022-01-27 16:45:41 +0100

    Cleanups (#5)
    * Major refactoring and cleanups

--- asciimol/app/colors.py
+++ asciimol/app/colors.py
@@ -1,53 +1,22 @@
 import curses
 
 
-class ColorDict:
-    def __init__(self):
-        self.active = False
-        self.dict = {}
-        self._cdict = {}
-        if curses.has_colors():
-            curses.use_default_colors()
-            from asciimol.app import data_colors, data_atoms
-            # Init all the required color pairs that were defined in the data file
-            cset = {}
-            for n, clabel in enumerate(data_colors):
-                r, g, b, x256, x8 = data_colors[clabel]
-                col = x256 if curses.COLORS >= 256 else x8
-                if col not in cset:
-                    cset[col] = n
+def init_curses_color_pairs(a):
+    if curses.has_colors():
+        curses.use_default_colors()
+        # Lazily init only colors as color pairs that are actually needed
+        pair = 1
+        cdict = dict()
+        for entry in a:
+            r, g, b, x256, x8 = entry
+            col = x256 if curses.COLORS >= 256 else x8
+            if str(col) not in cdict:
                 try:
-                    curses.init_pair(cset[col], col, -1)
+                    curses.init_pair(pair, col, -1)
+                    cdict[str(col)] = pair
+                    pair += 1
                 except curses.error:
-                    raise RuntimeError("Could not init pair %d with color %d" % (cset[col], col))
-                self._cdict[clabel] = cset[col]
-            self.active = True
-            # Translate all of the colors defined for the atom symbols to active curses ATTR
-            self.dict = {}
-            for key in data_atoms:
-                col = data_atoms[key][1]
-                if col in self._cdict:
-                    self.dict[key] = str(curses.color_pair(self._cdict[col]))
-
-    def __getitem__(self, item):
-        if len(self.dict) == 0:
-            return str(curses.color_pair(0))
-        try:
-            return self.dict[item]
-        except (KeyError, ValueError):
-            return str(curses.color_pair(0))
-
-# Old Code for custom color palette -- does not work on xterm :(
-#
-# for n, key in enumerate(data_colors):
-#     r, g, b = data_colors[key]
-#     curses.init_color(n + 1, round(r * 1000), round(g * 1000), round(b * 1000))
-#     curses.init_pair(n + 1, n + 1, curses.COLOR_BLACK)
-#     self._cdict[key] = n + 1
-# self.active = True
-# # Translate all of the colors defined for the atom symbols to active curses ATTR
-# self.dict = {}
-# for key in data_atoms:
-#     col = data_atoms[key][1]
-#     if col in self._cdict:
-#         self.dict[key] = str(curses.color_pair(self._cdict[col]))
+                    raise RuntimeError("Could not init pair %d with color %d" % (pair, col))
+            yield str(curses.color_pair(cdict[str(col)]))
+    else:
+        return ["0"] * len(a)
