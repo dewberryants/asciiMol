0a3e143a470b108faa9d5e8d21d34f64ccf9106a
Dominik Behrens <dewberryants@gmail.com>
2023-03-03 10:11:04 +0100

    Try to open _everything_ as xyz first

--- asciimol/app/io.py
+++ asciimol/app/io.py
@@ -16,57 +16,56 @@ except ImportError:
 
 
 def handle_io(input_string: str):
-    if (not use_ase and not use_rdkit) or input_string[-4:] == ".xyz":
-        try:
-            with open(input_string, "r") as handle:
-                return read_xyz(handle)
-        except (ValueError, FileNotFoundError):
-            print("ERROR: Could not read '%s' as .xyz file." % input_string)
-            if input_string[-4:] != ".xyz":
-                print("Consider installing the optional dependencies (ase, rdkit) for more formats!")
-            return None, None, None
-    elif use_ase and not use_rdkit:
-        try:
-            atms = read(input_string)
-        except FileNotFoundError:
+    try:
+        with open(input_string, "r") as handle:
+            return read_xyz(handle)
+    except (ValueError, FileNotFoundError):
+        if use_ase and not use_rdkit:
             try:
-                print("Searching '%s' on PubChem..." % input_string)
-                atms = pubchem_atoms_search(smiles=input_string)
-            except:
-                print("ERROR: Argument does not seem to be an existing file or SMILES string.")
-                return None, None, None
-        return [len(atms)], atms.get_positions(), atms.get_chemical_symbols()
-    elif use_rdkit and not use_ase:
-        try:
-            with open(input_string, "r") as handle:
-                return read_xyz(handle)
-        except ValueError:
-            print("ERROR: Could not read '%s' as xyz file." % input_string)
-            return None, None, None
-        except FileNotFoundError:
-            counts, pos, sym = read_smiles(input_string)
-            if not counts:
-                print("ERROR: File '%s' not found and not a valid SMILES code!" % input_string)
-                return None, None, None
-            return counts, pos, sym
-    else:  # Using both
-        try:
-            atms = read(input_string)
+                atms = read(input_string)
+            except FileNotFoundError:
+                try:
+                    print("Searching '%s' on PubChem..." % input_string)
+                    atms = pubchem_atoms_search(smiles=input_string)
+                except:
+                    print("ERROR: Argument does not seem to be an existing file or SMILES string.")
+                    return None, None, None
             return [len(atms)], atms.get_positions(), atms.get_chemical_symbols()
-        except FileNotFoundError:
-            counts, pos, sym = read_smiles(input_string)
-            if not counts:
-                print("ERROR: File '%s' not found and not a valid SMILES code!" % input_string)
-                return None, None, None
-            return counts, pos, sym
-        except KeyError:
-            # ASE struggles to read XYZ files with invalid atom symbols
-            # so try to read as xyz or fail
+        elif use_rdkit and not use_ase:
             try:
                 with open(input_string, "r") as handle:
                     return read_xyz(handle)
-            except (ValueError, FileNotFoundError):
-                print("ERROR: ASE could not open '%s' and could not read '%s' as simple .xyz file." % input_string)
+            except ValueError:
+                print("ERROR: Could not read '%s' as xyz file." % input_string)
+                return None, None, None
+            except FileNotFoundError:
+                counts, pos, sym = read_smiles(input_string)
+                if not counts:
+                    print("ERROR: File '%s' not found and not a valid SMILES code!" % input_string)
+                    return None, None, None
+                return counts, pos, sym
+        else:  # Using both
+            try:
+                atms = read(input_string)
+                return [len(atms)], atms.get_positions(), atms.get_chemical_symbols()
+            except FileNotFoundError:
+                counts, pos, sym = read_smiles(input_string)
+                if not counts:
+                    print("ERROR: File '%s' not found and not a valid SMILES code!" % input_string)
+                    return None, None, None
+                return counts, pos, sym
+            except KeyError:
+                # ASE struggles to read XYZ files with invalid atom symbols
+                # so try to read as xyz or fail
+                try:
+                    with open(input_string, "r") as handle:
+                        return read_xyz(handle)
+                except (ValueError, FileNotFoundError):
+                    print("ERROR: ASE could not open '%s' and could not read '%s' as simple .xyz file." % input_string)
+        print("ERROR: Could not read '%s' as .xyz file." % input_string)
+        if input_string[-4:] != ".xyz":
+            print("Consider installing the optional dependencies (ase, rdkit) for more formats!")
+            return None, None, None
 
 
 def read_smiles(smiles):
